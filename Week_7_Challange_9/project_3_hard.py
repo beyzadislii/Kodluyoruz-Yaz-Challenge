2e5e6204193344c8c28b0b70f52631d5f2692aa6
beyzadislii <138888991+beyzadislii@users.noreply.github.com>
2023-08-23 15:20:46 +0300

    Delete Week_7_Challange_9 directory

--- Week_7_Challange_9/project_3_hard.py
+++
@@ -1,6 +0,0 @@
-x=int(input("Enter a number: "))
-result=x**0.5
-if (result).is_integer()==True:
-  print(result)
-else:
-  print("Square root of a number is not an integer.")
--- Week_7_Challange_9/project_3_medium.py
+++
@@ -1,4 +0,0 @@
-x=(input("Enter your text: "))
-y=x.split()
-result=len(y)
-print(result)
